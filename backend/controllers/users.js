3ca0c0b67a47bfd64df61e691a3304908ce422c3
Ortem-T <84287305+Ortem-T@users.noreply.github.com>
2022-09-03 17:20:53 +0300

    feat: fix bag

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -124,6 +124,7 @@ module.exports.login = (req, res, next) => {
           maxAge: 3600000,
           httpOnly: true,
           sameSite: false,
+          secure: true,
         })
           .send({ message: 'Всё верно!' });
       });
