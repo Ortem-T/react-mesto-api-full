b823f9f5af0220886f8baf43882632664cfcce01
Ortem-T <84287305+Ortem-T@users.noreply.github.com>
2022-09-03 12:22:18 +0300

    feat: create domain name

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,5 @@
-export const BASE_URL = 'http://localhost:3000';
+// export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'mesto.ortem.nomoredomains.sbs';
 
 const handleResponse = (res) => {
   if (res.ok) {
