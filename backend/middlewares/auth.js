5b2399baa42b3092c348dc5acbf823df0adf53af
Ortem-T <84287305+Ortem-T@users.noreply.github.com>
2022-09-04 18:13:34 +0300

    feat: fix bag

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -3,14 +3,14 @@ const AuthErr = require('../errors/AuthErr_401');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
-  console.log(authorization);
-  if (!authorization) {
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
     throw new AuthErr('Необходима авторизация');
   }
 
   const token = authorization.replace('Bearer ', '');
   let payload;
-
+  console.log(token);
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
