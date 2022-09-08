59352e1abddcc50b2d93dc0a625c5556d0caf9af
Ortem-T <84287305+Ortem-T@users.noreply.github.com>
2022-09-08 22:38:04 +0300

    feat: fix bag

--- backend/app.js
+++ backend/app.js
@@ -34,11 +34,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
 app.use(requestLogger);
 app.use('*', cors(options));
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 app.post(
   '/signin',
